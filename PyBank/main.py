fcd8a6e66c550d58b0f4e5b4827aba42b01ca75e
Whitney Taylor <Whitney.taylor77@gmail.com>
2022-10-03 17:31:04 -0400

    [Python-Challenge]

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,58 @@
+print("Financial Analysis")
+print("-----------------------")
+ 
+from datetime import date
+from operator import index
+import os
+import csv
+csvpath=os.path.join('budget_data.csv')
+with open(csvpath,encoding="utf8") as csvfile:
+    csv_reader = csv.reader(csvfile,delimiter=",")
 
+with open('budget_data.csv', newline="") as f:
+    reader= csv.reader(f)
+    data = list(reader)
+   
+reader = csv.reader(open("budget_data.csv"))
+lines = len(list(reader))-1
+print(f"Total Months: {lines}")
+ 
+sum = 0
+for index, row in enumerate(data):
+    if index != 0:
+        current = (row[1])
+        sum += int(current)
+ 
+ 
+print(f"Total: {sum}")
+ 
+index = 0
+totalchange = 0
+for index, row in enumerate(data):
+    if(index > 1):
+        prevRow = data[index-1]
+        x1 = prevRow[1]
+        x2 = row[1]
+        change = int(x2) - int(x1)
+        totalchange = totalchange + change
+ 
+avg_change = totalchange/lines
+ 
+print(f"Average Change: {avg_change}")
+ 
+index = 0
+greatest_increase = 0
+greatest_increase_index = 0
+for index, row in enumerate(data):
+    if index != 0:
+        current_value = row[1]
+        current_value_int = int(current_value)
+        if current_value_int > greatest_increase:
+            greatest_increase = current_value_int
+            greatest_increase_index = index
+ 
+greatest_increase_line_data = data[greatest_increase_index]
+greatest_increase_value = greatest_increase_line_data[1]
+greatest_increase_date = greatest_increase_line_data[0]
+print(greatest_increase_value, greatest_increase_date)
+print(f"Greatest Increase In Profits: ")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,61 @@
+import os
+import csv
+
+election_data_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'election_data.csv')
+
+totalVotes = 0 
+
+votesPerCandidate = {}
+
+
+with open(election_data_csv, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+
+    csv_header = next(csvreader)
+
+
+    for row in csvreader:
+        totalVotes += 1
+        if row[2] not in votesPerCandidate:
+            votesPerCandidate[row[2]] = 1
+        else:
+            votesPerCandidate[row[2]] += 1   
+        
+        
+
+
+print("Election Results")
+print("-------------------------")
+print("Total Votes: " + str(totalVotes))
+print("-------------------------")
+
+for candidate, votes in votesPerCandidate.items():
+    print(candidate + ": " + "{:.3%}".format(votes/totalVotes) + "   (" +  str(votes) + ")")
+    
+print("-------------------------") 
+
+winner = max(votesPerCandidate, key=votesPerCandidate.get)
+
+print(f"Winner: {winner}")
+
+f = open("election_results.txt", "w")
+f.write("Election Results")
+f.write('\n')
+f.write("-------------------------")
+f.write('\n')
+f.write("Total Votes: " + str(totalVotes))
+f.write('\n')
+f.write("-------------------------")
+f.write('\n')
+
+for candidate, votes in votesPerCandidate.items():
+    f.write(candidate + ": " + "{:.3%}".format(votes/totalVotes) + "   (" +  str(votes) + ")")
+    f.write('\n')
+  
+f.write("-------------------------") 
+f.write('\n')
+f.write(f"Winner: {winner}")
+f.write('\n')
 
